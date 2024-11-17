10681253bbaaf142409397c81a0430dcd6e030e6
K-Praveen22 <praveen22daya@gmail.com>
2024-11-17 21:14:42 +0530

    Update ENUM_WITHOUT_TYPEDEF.sv

--- DATA TYPES/ENUM_WITHOUT_TYPEDEF.sv
+++ DATA TYPES/ENUM_WITHOUT_TYPEDEF.sv
@@ -3,10 +3,10 @@ module enum_ex();
         BRINJAL,
         POTOTO,
         ONION,
-        TOMOTO} veg_e;
+        TOMATO} veg_e;
   
   initial begin
-    veg_e=ONION;
+    veg_e=TOMATO;
     $display("Element is %s and its value is %0d",veg_e.name(),veg_e);
      end
 endmodule
