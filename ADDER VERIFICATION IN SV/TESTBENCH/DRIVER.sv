6f55acdc591691f875033ef4de9aae9010d06b5a
K-Praveen22 <praveen22daya@gmail.com>
2024-11-26 09:28:20 +0530

    Create DRIVER.sv

---
+++ ADDER VERIFICATION IN SV/TESTBENCH/DRIVER.sv
@@ -0,0 +1,22 @@
+
+class driver;
+  transaction pkt;
+  mailbox mail;
+  virtual operation vintf;
+  
+  
+  function new(virtual operation vintf,mailbox mail);
+    this.vintf=vintf;
+    this.mail= mail;
+  endfunction
+  
+  task drive;
+    repeat(10)begin
+      pkt=new();
+      mail.get(pkt);
+      vintf.a=pkt.a;
+      vintf.b=pkt.b;
+     pkt.c=vintf.c;
+    end
+  endtask
+endclass
