026da20f98355c89c01b84387aa33766b42c1e07
K-Praveen22 <praveen22daya@gmail.com>
2024-12-03 23:27:46 +0530

    Create CONSTRAINTS INSIDE RANGE.sv

---
+++ CONSTRAINTS/CONSTRAINTS INSIDE RANGE.sv
@@ -0,0 +1,12 @@
+// Code your design here
+class base;
+rand bit[8:0] A;
+constraint range { A inside {[0:63], [128:191]};}
+endclass
+module test;
+base h=new;
+initial repeat(10) begin
+assert(h.randomize);
+$display("%d",h.A);
+end
+endmodule
